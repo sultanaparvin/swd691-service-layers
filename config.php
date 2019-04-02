9639f66d829f13aa06b0fe657e9455c83775de90
Sultana Parvin <Simy4825@gmail.com>
2019-04-02 19:38:18 -0400

    1.Created a config file including connection to the database. Created a blank class for user

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+//Database conection credentials
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = '';
+
+//Connect to the database
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+//Include classes
+require_once('/classes/user.php');
