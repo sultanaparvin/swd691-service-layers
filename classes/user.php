6efa36e704101ed21fc399c3886e8f6b23feb1de
Sultana Parvin <Simy4825@gmail.com>
2019-04-02 20:08:05 -0400

    Added getAll, getById, save and delete methods to the user class

--- classes/user.php
+++ classes/user.php
@@ -7,6 +7,7 @@ class User{
     private $email;
     private $privilege;
 
+    //Privilege types
     public static $PRIVILEGE_TYPES = array('Manager','Tester','Developer');
     public static $PRIVILEGE_MANAGER   = 1;
     public static $PRIVILEGE_TESTER    = 2;
@@ -57,5 +58,50 @@ class User{
     }
     public function setPrivilege($privilege){
         $this->privilege = $privilege;
+    }
+
+    //Get all the users
+    public function getAll(){
+        $output = array();
+        global $conn;
+        $sql = "SELECT * FROM users";
+        $res = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($res) > 0){ //Check to make sure table is not empty
+            while($row = mysqli_fetch_object($res)){
+                $output[] = new User($row->id, $row->name, $row->username, $row->password, $row->email, $row->privilege);
+            }
+        }
+        return $output;
+    }
+
+    //Get a user by id
+    public function getById($id){
+        $output = false;
+        global $conn;
+        $sql = "SELECT * FROM users WHERE `id`=".$id;
+        $res = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($res) > 0){ //Check to make sure table is not empty
+            $output = new User($row->id, $row->name, $row->username, $row->password, $row->email, $row->privilege);
+        }
+        return $output;
+    }
+
+    //Save function handle both insert and update.
+    public function save(){
+        global $conn;
+        if($this->id === null){ //Insert
+            $sql = "INSERT INTO `users` (`name`,`username`,`password`,`email`,`privilege`)VALUES('".$this->name.""','".$this->username."','".$this->password."','".$this->email."','".$this->privilege."')";
+            mysqli_query($conn,$sql);
+        }else{ //Update
+            $sql = "UPDATE `users` SET `name`='".$this->name."', `username`='".$this->username."' ,`password`='".$this->password."' ,`email`='".$this->email."' ,`privilege`='".$this->privilege."' WHERE `id`=".$this->id;
+            mysqli_query($conn,$sql);
+        }
+    }
+
+    //Delete function removes a user
+    public function delete(){
+        global $conn;
+        $sql = "DELETE FROM `users` WHERE `id`=".$this->id;
+        mysqli_query($conn,$sql);
     }
 }
