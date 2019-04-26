32e83c15cc32e1ccc5040c1e816c4b9dd5b04eaa
Sultana Parvin <Simy4825@gmail.com>
2019-04-25 21:49:11 -0400

    1.Added production environment database credentials. 2.Added production domain and users to readme file

--- README.md
+++ README.md
@@ -4,3 +4,22 @@
 
 This repository serves as a codebase for development of a service layer for "Website Testing Dashboard" project. The service layer is the layer located between the user interface and database. It transfers the data between user interface and database and vice versa.
 
+
+## Production:
+
+User Interface: http://www.testcase.website/ <br>
+API: http://www.testcase.website/api/
+
+## Sample Users:
+
+**Manager**<br>
+User: manager<br>
+Pass: manager
+
+**Tester**<br>
+User: tester<br>
+Pass: tester
+
+**Developer**<br>
+User: developer<br>
+Pass: developer
--- config.php
+++ config.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
 
-//Database conection credentials
+// Local Database conection credentials
+// $db_host = 'localhost';
+// $db_user = 'root';
+// $db_pass = '';
+// $db_name = 'testing_dashboard';
+
+// Production Database conection credentials
 $db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'testing_dashboard';
+$db_user = 'ndzwgvra_user';
+$db_pass = 'HiAW+qU4?LD&';
+$db_name = 'ndzwgvra_db';
+
 
 //Connect to the database
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
@@ -13,5 +20,5 @@ $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 //Include classes
 require_once('classes/user.php');
 require_once('classes/project.php');
-require_once('classes/Comment.php');
-require_once('classes/Testcase.php');
+require_once('classes/comment.php');
+require_once('classes/testcase.php');
